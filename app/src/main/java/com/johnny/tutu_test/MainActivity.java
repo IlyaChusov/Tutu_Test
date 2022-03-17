752c668e62df598e40a802a1d904820c94a565d9
Chusov Ilya <ilyachusov@gmail.com>
2022-03-17 13:08:37 +0300

    Fetching pokemon list added

--- app/src/main/java/com/johnny/tutu_test/MainActivity.java
+++ app/src/main/java/com/johnny/tutu_test/MainActivity.java
@@ -1,20 +1,30 @@
 package com.johnny.tutu_test;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.johnny.tutu_test.databinding.ActivityMainBinding;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,11 +36,15 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private JSONArray pokemonList = new JSONArray();
+    private PokemonAdapter pokemonAdapter = new PokemonAdapter();
+    private final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +52,16 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.toolbar);
+
+        binding.recycler.setLayoutManager(new LinearLayoutManager(this));
+        binding.recycler.setAdapter(pokemonAdapter);
+
+
         FetchData fetchData = new FetchData();
         fetchData.start();
+
+
     }
 
     class FetchData extends Thread {
@@ -63,16 +83,69 @@ public class MainActivity extends AppCompatActivity {
                 if (!data.isEmpty()) {
                     JSONObject dataObject = new JSONObject(data);
                     JSONArray dataArray = dataObject.getJSONArray("results");
-                    dataArray.get(0);
+                    if (dataArray.length() != 0) {
+                        pokemonList = dataArray;
+                        handler.post(() -> pokemonAdapter.notifyDataSetChanged());
+
+                    }
+                    else throw new JSONException("pokemonList is empty");
                 }
                 else {
-
+                    throw new JSONException("cannot get JSONArray");
                 }
 
             } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
 
+        }
+    }
+
+    private class PokemonAdapter extends RecyclerView.Adapter<PokemonHolder> {
+        @NonNull
+        @Override
+        public PokemonHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new PokemonHolder(getLayoutInflater(), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PokemonHolder holder, int position) {
+            try {
+                holder.bind(pokemonList.getJSONObject(position));
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "KEKHands", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return pokemonList.length();
+        }
+    }
+
+    private class PokemonHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private JSONObject pokemon;
+
+        public PokemonHolder(@NonNull @NotNull LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item, parent, false));
+
+            itemView.setOnClickListener(this);
+        }
+
+        private void bind(@NonNull JSONObject pokemon) {
+            this.pokemon = pokemon;
+
+            try {
+                ((TextView) itemView.findViewById(R.id.pokemonName)).setText(pokemon.getString("name"));
+            }
+            catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "KEKHands", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "PogU", Toast.LENGTH_LONG).show();
         }
     }
 }
